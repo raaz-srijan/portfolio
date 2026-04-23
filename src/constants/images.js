b8a81805dcb096ffa25fbce5b81bd8564eac0750
raaz-srijan <srijannospace@gmail.com>
2026-04-23 08:07:22 +0545

    added the image

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,10 @@
-export { default as hosp1 } from "../assets/hosp1.png"
-export { default as hosp2 } from "../assets/hosp2.png"
-export { default as hosp3 } from "../assets/hosp3.png"
-export { default as hosp4 } from "../assets/hosp4.png"
+export { default as hosp1 } from "../assets/hosp1.png";
+export { default as hosp2 } from "../assets/hosp2.png";
+export { default as hosp3 } from "../assets/hosp3.png";
+export { default as hosp4 } from "../assets/hosp4.png";
 
-export { default as book1 } from "../assets/book1.png"
-export { default as book2 } from "../assets/book2.png"
-export { default as book3 } from "../assets/book3.png"
-export { default as book4 } from "../assets/book4.png"
+export { default as book1 } from "../assets/book1.png";
+export { default as book2 } from "../assets/book2.png";
+export { default as book3 } from "../assets/book3.png";
+export { default as book4 } from "../assets/book4.png";
+export { default as me } from "../assets/me.JPG";
